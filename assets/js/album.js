efcce1114a2965b86e4e3c78cf9f2cbfc4593dc6
FlorLizardo <florencializardo7@gmail.com>
2023-12-18 23:10:42 +0100

    modifiche style homePage

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,72 +1,146 @@
-const myUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/62819462";
+// // fetch jumbotron singola canzone
+// const myUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/62819462";
 
-const getJb = () => {
-  fetch(myUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-      getALbum(data)
-      getTracksAlbum(data)
-    })
-    .catch((error) => console.error("Error:", error));
-};
+// const getJb = () => {
+//   fetch(myUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data)
+//       getALbum(data)
+//       getTracksAlbum(data)
+//     })
+//     .catch((error) => console.error("Error:", error));
+// };
 
 const params = new URLSearchParams(location.search)
 const id = params.get('id');
 
-const getALbum = (data) => {
-  const rowAlbum = document.getElementById('albumtron');
+// const getALbum = (data) => {
+//   const rowAlbum = document.getElementById('albumtron');
 
-  let album = 
-  `<div class="col-xl-3">
-  <img
-    src="${data.cover}"
-    width="190"
-    height="190"
-    class="object-fit-cover ms-0 p-2"
-  />
+//   let album = 
+//   `<div class="col-xl-3">
+//   <img
+//     src="${data.cover}"
+//     width="190"
+//     height="190"
+//     class="object-fit-cover ms-0 p-2"
+//   />
+// </div>
+// <div class="col-xl-9 px-3">
+//   <div>
+//     <p class="h6">ALBUM</p>
+//     <h1 class="display-4">${data.title}</h1>
+//     <p class="h6">
+//     <img src="${data.contributors[0].picture_small}" width="30px" height="30px" class="rounded-circle"> ${data.artist.name} - ${data.release_date} - ${data.nb_tracks} brani -
+//       ${data.duration}
+//     </p>
+//   </div>
+// </div>` 
+//   rowAlbum.innerHTML = album;
+// }
+
+// const getTracksAlbum = (data) => {
+
+//    for(let i = 0; i <= data.tracks.data.length; i++) {
+//     const rowTracks = document.getElementById('tracklist');
+//     let track = 
+//     `<div class="col-xl-1 text-secondary d-flex justify-content-end align-items-center">
+//                ${data.nb_tracks}
+//               </div>
+//               <div class="col-xl-7">
+//                 <div class="row">
+//                   <div class="col-12 h6 text-light text-capitalize">${data.tracks.data[i].title}</div>               
+//                   <div class="col h6 text-secondary text-capitalize">${data.artist.name}</div>
+//                 </div>
+//               </div>
+
+//               <div class="col-xl-3 text-secondary d-flex align-items-center">${data.tracks.data[i].rank}</div>
+//               <div class="col-xl-1 text-secondary d-flex align-items-center">${data.tracks.data[i].duration}</div>`
+// //   `<div>
+// //   <p>${data.tracks.data[i].title}</p>
+// //   <small>${data.artist.name}</small>
+// //   <small>${data.tracks.data[i].rank}</small>
+// //   <small>${data.tracks.data[i].duration}</small>
+// // </div>`
+ 
+//   rowTracks.innerHTML = track;
+//    }
+// }
+
+
+//fetch tracklist album dinamica  dalla homepage
+
+const secondUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+
+
+const getTrackList = () => {
+ fetch(secondUrl + id)
+   .then((response) => response.json())
+   .then((data) => {
+       console.log(data)
+     tracklist(data)
+     getSong(data)
+   })
+   .catch((error) => console.error("Error:", error));
+};
+
+
+
+const tracklist = (data) => {
+ const rowAlbum = document.getElementById('albumtron');
+
+ let album = 
+ `<div class="col-xl-3">
+ <img
+   src="${data.cover}"
+   width="190"
+   height="190"
+   class="object-fit-cover ms-0 p-2"
+ />
 </div>
 <div class="col-xl-9 px-3">
-  <div>
-    <p class="h6">ALBUM</p>
-    <h1 class="display-4">${data.title}</h1>
-    <p class="h6">
-    <img src="${data.contributors[0].picture_small}" width="30px" height="30px" class="rounded-circle"> ${data.artist.name} - ${data.release_date} - ${data.nb_tracks} brani -
-      ${data.duration}
-    </p>
-  </div>
+ <div>
+   <p class="h6">ALBUM</p>
+   <h1 class="display-4">${data.title}</h1>
+   <p class="h6">
+   <img src="${data.contributors[0].picture_small}" width="30px" height="30px" class="rounded-circle"> ${data.artist.name} - ${data.release_date} - ${data.nb_tracks} brani -
+     ${data.duration}
+   </p>
+ </div>
 </div>` 
-  rowAlbum.innerHTML = album;
+ rowAlbum.innerHTML = album;
 }
 
-const getTracksAlbum = (data) => {
+const getSong = (data) => {
 
-   for(let i = 0; i <= data.tracks.data.length; i++) {
-    const rowTracks = document.getElementById('tracklist');
-    let track = 
-    `<div class="col-xl-1 text-secondary d-flex justify-content-end align-items-center">
-               ${data.nb_tracks}
-              </div>
-              <div class="col-xl-7">
-                <div class="row">
-                  <div class="col-12 h6 text-light text-capitalize">${data.tracks.data[i].title}</div>               
-                  <div class="col h6 text-secondary text-capitalize">${data.artist.name}</div>
-                </div>
-              </div>
+  for(let i = 0; i <= data.tracks.data.length; i++) {
+   //const nbTrack = 1;
+   
+   const rowTracks = document.getElementById('tracklist');
+   let track = 
+   `<div class="col-xl-1 text-secondary d-flex justify-content-end align-items-center">
+              
+             </div>
+             <div class="col-xl-7">
+               <div class="row">
+                 <div class="col-12 h6 text-light text-capitalize">${data.tracks.data[i].title}</div>               
+                 <div class="col h6 text-secondary text-capitalize">${data.artist.name}</div>
+               </div>
+             </div>
 
-              <div class="col-xl-3 text-secondary d-flex align-items-center">${data.tracks.data[i].rank}</div>
-              <div class="col-xl-1 text-secondary d-flex align-items-center">${data.tracks.data[i].duration}</div>`
-//   `<div>
-//   <p>${data.tracks.data[i].title}</p>
-//   <small>${data.artist.name}</small>
-//   <small>${data.tracks.data[i].rank}</small>
-//   <small>${data.tracks.data[i].duration}</small>
-// </div>`
-  
-  rowTracks.innerHTML = track;
-   }
+             <div class="col-xl-3 text-secondary d-flex align-items-center">${data.tracks.data[i].rank}</div>
+             <div class="col-xl-1 text-secondary d-flex align-items-center">${data.tracks.data[i].duration}</div>`
+
+ 
+ rowTracks.innerHTML += track;
+  }
 }
+
+
+
 
 window.onload = () => {
-  getJb();
+ //getJb();
+ getTrackList();
 }
--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,29 +1,29 @@
 const test = "https://striveschool-api.herokuapp.com/api/deezer/album/62819462";
 
 const getDisplayJb = () => {
-  fetch(test)
-    .then((response) => response.json())
-    .then((data) => displayJumbotron(data))
-    .catch((error) => console.error("Error:", error));
+	fetch(test)
+		.then((response) => response.json())
+		.then((data) => displayJumbotron(data))
+		.catch((error) => console.error("Error:", error));
 };
 
 function displayJumbotron(data) {
-  let containerJumbotron = document.getElementById("jumbotron");
+	let containerJumbotron = document.getElementById("jumbotron");
 
-  containerJumbotron.innerHTML = `
-    <div class="row" >
-      <div class="col-xl-3">
+	containerJumbotron.innerHTML = `
+    <div class="row bg-jumbotron py-3 ps-5" >
+      <div class="col-xl-2 d-flex align-items-center justify-content-end">
         <img
           src="${data.cover}"
-          width="190"
-          height="190"
-          class="object-fit-cover ms-0 p-2"
+          width="170"
+          height="170"
+         
         />
       </div>
-      <div class="col-xl-9 px-3">
+      <div class="col-xl-9">
         <div>
           <p class="h6">ALBUM</p>
-          <a href="../../album.html?id=${data.id}" class="text-decoration-none"><h1 class="display-4 text-light">${data.title}</h1></a>
+          <a href="../../album.html?id=${data.id}" class="text-decoration-none"><h1 class="display-4 text-light fw-bold">${data.title}</h1></a>
           <a href="../../artista.html?id=${data.contributors[0].id}" class="text-decoration-none"><p class="h6 text-light">${data.artist.name}</p></a>
           <p class="h6">Ascolta il nuovo singolo di ${data.artist.name}</p>
         </div>
@@ -31,7 +31,7 @@ function displayJumbotron(data) {
         <div class="my-3">
           <a
             href="#"
-            class="bg-success text-black rounded-5 px-4 py-2 fw-bold text-decoration-none"
+            class="btn-play text-black rounded-5 px-4 py-2 fw-bold text-decoration-none"
             >Play</a
           >
           <a
@@ -58,34 +58,27 @@ function displayJumbotron(data) {
 
 /*CARDS LUNGHINE */
 const cardOrizzontali = [
-  "363906907",
-  "510894151",
-  "52845302",
-  "317985097",
-  "303572557",
-  "211834222",
+	"363906907",
+	"510894151",
+	"52845302",
+	"317985097",
+	"303572557",
+	"211834222",
 ];
 
 function fetchCardOrizzontali() {
-  
-    
-  
-  for (const playlistId of cardOrizzontali) {
-    fetch(
-      `https://striveschool-api.herokuapp.com/api/deezer/album/${playlistId}`
-    )
-      .then((response) => response.json())
-      .then((data) => cardPlaylist(data));
-  }
-
+	for (const playlistId of cardOrizzontali) {
+		fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${playlistId}`)
+			.then((response) => response.json())
+			.then((data) => cardPlaylist(data));
+	}
 }
 
 function cardPlaylist(data) {
-  let rowPlaylist = document.getElementById('cardOblunghe');
-  
-  let stampina =
-   `
-  <div class="col-xl-12 d-flex column-gap-2 my-1 bg-secondary p-0 playlistCol">
+	let rowPlaylist = document.getElementById("cardOblunghe");
+
+	let stampina = `
+  <div class="col-xl-12 d-flex column-gap-2 my-1 bg-card-orizz p-0 playlistCol">
   <div class="p-0">
             <img
             src="${data.cover_small}"
@@ -97,131 +90,96 @@ function cardPlaylist(data) {
             <div class="d-flex align-items-center">
             <p class="h6">${data.title}</p>
             </div>
-            </div>`
-;
-            rowPlaylist.innerHTML += stampina;
-
-
-
-  
-};
-
-
+            </div>`;
+	rowPlaylist.innerHTML += stampina;
+}
 
 /*CARDS ALTINE */
 const cardVerticali = [
-  "363906907",
-  "510894151",
-  "52845302",
-  "317985097",
-  "52845302",
-  "52845302",
-  "52845302",
-  "317985097",
-  "52845302",
-  "52845302",
-  "317985097",
-  "52845302"
-  
+	"363906907",
+	"510894151",
+	"52845302",
+	"317985097",
+	"52845302",
+	"52845302",
+	"52845302",
+	"317985097",
+	"52845302",
+	"52845302",
+	"317985097",
+	"52845302",
 ];
 
-
 function fetchCardVerticali() {
-  
-    
-  
-  for (const playlistId of cardVerticali) {
-    fetch(
-      `https://striveschool-api.herokuapp.com/api/deezer/album/${playlistId}`
-    )
-      .then((response) => response.json())
-      .then((data) => stampaCardVerticali(data));
-  }
-
+	for (const playlistId of cardVerticali) {
+		fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${playlistId}`)
+			.then((response) => response.json())
+			.then((data) => stampaCardVerticali(data));
+	}
 }
 
 function stampaCardVerticali(data) {
-  let cardAltine = document.getElementById('cardAltine');
-  
-  let stampina =
-   `
+	let cardAltine = document.getElementById("cardAltine");
+
+	let stampina = `
    <div class="col-xl-2">
-   <div class="card bg-secondary text-white">
-     <img
+   <div class="card bg-card-verticali text-white">
+   <a href="../../album.html?id=${data.id}" class="text-decoration-none"><img
        src="${data.cover_medium}"
        class="card-img-top object-fit-cover rounded-bottom m-3"
        alt="Immagine Playlist 2"
        style="height: 110px; width: 110px"
-     />
-     <div class="card-body">
-       <p class="card-text h6 text-light" style="font-size: 12px">
+     /></a>
+     <div class="card-body pt-0">
+     <a href="../../album.html?id=${data.id}" class="text-decoration-none"> <p class="card-text h6 text-light pb-1" style="font-size: 12px">
        ${data.title}
-       </p>
-       <p class="card-text text-light" style="font-size: 10px">
+       </p></a>
+       <a href="../../artista.html?id=${data.id}" class="text-decoration-none"><p class="card-text text-light" style="font-size: 10px">
        ${data.artist.name}
-       </p>
+       </p></a>
      </div>
    </div>
- </div>`
-;
-            cardAltine.innerHTML += stampina;
-
-
-
-  
-};
-
-
-
-
-
+ </div>`;
+	cardAltine.innerHTML += stampina;
+}
 
 /*CARDS ARTISTI */
 const cardArtisti = [
-  "5695",
-  "4331",
-  "14585",
-  "470538",
-  "65682",
-  "7219246",
-  "5695",
-  "4331",
-  "14585",
-  "470538",
-  "65682",
-  "7219246"
-  
+	"5695",
+	"4331",
+	"14585",
+	"470538",
+	"65682",
+	"7219246",
+	"5695",
+	"4331",
+	"14585",
+	"470538",
+	"65682",
+	"7219246",
 ];
 
-
 function fetchCardArtisti() {
-  
-    
-  
-  for (const artistId of cardArtisti) {
-    fetch(
-      `https://striveschool-api.herokuapp.com/api/deezer/artist/${artistId}`
-    )
-      .then((response) => response.json())
-      .then((data) => stampaCardArtisti(data));
-  }
-
+	for (const artistId of cardArtisti) {
+		fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${artistId}`)
+			.then((response) => response.json())
+			.then((data) => stampaCardArtisti(data));
+	}
 }
 
 function stampaCardArtisti(data) {
-  let cardCantanti = document.getElementById('cardArtisti');
-  
-  let stampina =
-   `
+	let cardCantanti = document.getElementById("cardArtisti");
+
+	let stampina = `
    <div class="col-xl-2">
-   <div class="card bg-secondary text-white">
+   <div class="card bg-card-verticali text-white">
      <img
        src="${data.picture_medium}"
        class="card-img-top object-fit-cover rounded-circle m-3"
        alt="Immagine Playlist 2"
        style="height: 110px; width: 110px"
      />
-     <div class="card-body">
+     <div class="card-body pt-0">
      <p class="card-text text-light h6" style="font-size: 12px">
        ${data.name}
        </p>
@@ -231,24 +189,13 @@ function stampaCardArtisti(data) {
        
      </div>
    </div>
- </div>`
-;
-            cardCantanti.innerHTML += stampina;
-
-
-
-  
-};
-
-
-
-
-
-
+ </div>`;
+	cardCantanti.innerHTML += stampina;
+}
 
 window.onload = () => {
-  getDisplayJb();
-  fetchCardOrizzontali();
-  fetchCardVerticali();
-  fetchCardArtisti();
+	getDisplayJb();
+	fetchCardOrizzontali();
+	fetchCardVerticali();
+	fetchCardArtisti();
 };
